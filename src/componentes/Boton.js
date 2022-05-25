9924eb8834211c6792bbad234b03e7485cee0b5f
elesine <danitza.canasa@gmail.com>
2022-05-25 17:50:27 -0500

    functionality

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import '../hojas-de-estilo/Boton.css'
 
 function Boton(props) {
@@ -8,9 +8,10 @@ function Boton(props) {
 	};
 
   return (
-    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' :  ''}`.trimEnd()}>
+    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' :  ''}`.trimEnd()} 
+			   onClick={() => props.manejarClic(props.children)}>
 			{props.children}
-	  </div>
+		</div>
   );
 }
 
--- src/componentes/Pantalla.js
+++ src/componentes/Pantalla.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 import '../hojas-de-estilo/Pantalla.css';
 
 const Pantalla = ({ input }) => (
-  <div className="input">
+  <div className='input'>
 		{input}
 	</div>
 );
